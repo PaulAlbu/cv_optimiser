af28599a030dfe423138a2e97def5255b90a5bda
Paul Albu <paul.i.albu@gmail.com>
2025-06-19 18:27:49 +0300

    set up app.prop file

---
+++ src/main/java/dev/paul/cv_ai/ChatController.java
@@ -0,0 +1,24 @@
+package dev.paul.cv_ai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/dad")
+    public String generate(@RequestParam(value = "message", defaultValue = "Tell me a dad joke") String message) {
+        return chatClient.prompt()
+                .user(message)
+                .call()
+                .content();
+    }
+
+}
---
+++ src/test/java/dev/paul/cv_ai/CvAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.paul.cv_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
